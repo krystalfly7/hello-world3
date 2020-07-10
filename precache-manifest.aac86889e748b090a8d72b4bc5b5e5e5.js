11142788f696b295f06c2bd09c4031dee7beb508
hellokrystalLi <pplfly@163.com>
2020-07-10 20:26:19 +0800

    test

---
+++ precache-manifest.aac86889e748b090a8d72b4bc5b5e5e5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73cb172ffaf7fcc8cde9",
+    "url": "/hello-world2/'/css/about.b78b6887.css"
+  },
+  {
+    "revision": "e571c5dd3f4787b5aeca",
+    "url": "/hello-world2/'/css/app.bb649e29.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/hello-world2/'/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "8263c3994c25fc7393ae0b5730c09286",
+    "url": "/hello-world2/'/index.html"
+  },
+  {
+    "revision": "73cb172ffaf7fcc8cde9",
+    "url": "/hello-world2/'/js/about.982745f8.js"
+  },
+  {
+    "revision": "e571c5dd3f4787b5aeca",
+    "url": "/hello-world2/'/js/app.310ca09d.js"
+  },
+  {
+    "revision": "1749a7d14aa29e251baa",
+    "url": "/hello-world2/'/js/chunk-vendors.c97a7ce9.js"
+  },
+  {
+    "revision": "264981e96c1c00c28025d28a1a40ecb1",
+    "url": "/hello-world2/'/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/hello-world2/'/robots.txt"
+  }
+]);
