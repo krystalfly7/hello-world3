d16808465c8b5f80239914451ef66f05ddb2622a
hellokrystalLi <pplfly@163.com>
2020-07-13 14:51:48 +0800

    test

--- precache-manifest.063c4a6459c93b8b347bef24cdd2bca6.js
+++ precache-manifest.063c4a6459c93b8b347bef24cdd2bca6.js
@@ -4,15 +4,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "css/about.e9da198c.css"
   },
   {
-    "revision": "d8b399c66ca9dd0716dd",
-    "url": "css/app.bb649e29.css"
+    "revision": "059eac43af318bfc801f",
+    "url": "css/app.f2c274ef.css"
   },
   {
     "revision": "82b9c7a5a3f405032b1db71a25f67021",
     "url": "img/logo.82b9c7a5.png"
   },
   {
-    "revision": "9f2603a584a4d8c4075ce825f4835e91",
+    "revision": "c4c29a488fc14997f7761d37a9a723f2",
     "url": "index.html"
   },
   {
@@ -20,15 +20,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "js/about.719db787.js"
   },
   {
-    "revision": "d8b399c66ca9dd0716dd",
-    "url": "js/app.fe0733ea.js"
+    "revision": "059eac43af318bfc801f",
+    "url": "js/app.b6fe417c.js"
   },
   {
     "revision": "1749a7d14aa29e251baa",
     "url": "js/chunk-vendors.c97a7ce9.js"
   },
   {
-    "revision": "264981e96c1c00c28025d28a1a40ecb1",
+    "revision": "d7eb58804a21a80ca07a1a2d04ccff97",
     "url": "manifest.json"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,10 +14,10 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.e836310d53fc3eba49b4b45b038bea22.js"
+  "precache-manifest.063c4a6459c93b8b347bef24cdd2bca6.js"
 );
 
-workbox.core.setCacheNameDetails({prefix: "hello-world2"});
+workbox.core.setCacheNameDetails({prefix: "hello-world3"});
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
